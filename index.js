0be1890f62936928917eb68b174b5aed4495d012
wdbrow11 <will.brown.hc@gmail.com>
2018-05-01 00:14:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const currentUser = "Will";
 const welcomeMessage = (`Welcome to Flatbook, ${currentUser} !`);
-const excitedWelcomeMessage = welcomeMessage.toUppercase()
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
